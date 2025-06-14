9fea4626a298d1bc7c2081c09a77b3cc18a7bf18
princejeanluc <princejeanluc.denteppe@gmail.com>
2025-06-14 14:07:00 +0100

    updating urls.py

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from core.router import router  # ou depuis ton app API
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-]
