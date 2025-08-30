83500f533ee2ea594c3668d836913913227b7a15
princejeanluc <princejeanluc.denteppe@gmail.com>
2025-08-30 17:29:31 +0100

    retify google client id and secret in env

--- config/settings.py
+++ config/settings.py
@@ -171,8 +171,8 @@ SOCIALACCOUNT_PROVIDERS = {
         "SCOPE": ["profile", "email"],
         "AUTH_PARAMS": {"access_type": "online"},
         "APP": {
-            "client_id": os.getenv("GOOGLE_CLIENT_ID"),
-            "secret": os.getenv("GOOGLE_CLIENT_SECRET"),
+            "client_id": os.getenv("GOOGLE_APP_CLIENT_ID"),
+            "secret": os.getenv("GOOGLE_APP_SECRET"),
             "key": ""
         }
     }
